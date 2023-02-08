3e6763273f89e51f62b8d1a12208288807ef630a
Katarzyna KLEPACKA <kklepacka@nx-domain.lan>
2023-02-08 14:06:12 +0100

    commit script

--- src/test/java/testcscu4/StepDefinition.java
+++ src/test/java/testcscu4/StepDefinition.java
@@ -3,16 +3,120 @@ package testcscu4;
 import io.cucumber.java.en.Given;
 
 public class StepDefinition {
-    @Given("Test ` echo test `")
+    @Given("test '")
     public void test1() {
-        System.err.println("Test ` echo test `");
+        System.out.println("Test");
     }
-    @Given("Test '")
-    public void test2() {
-        System.err.println("Test '");
+    @Given("Test ` echo test `")
+public void test2() {
+    System.out.println("Test");
+}
+
+    @Given("Test $test")
+    public void test3() {
+        System.out.println("Test");
+    }
+
+    @Given("Test & echo toto")
+    public void test4() {
+        System.out.println("Test");
+    }
+
+    @Given("Test ; echo toto")
+    public void test5() {
+        System.out.println("Test");
+    }
+
+    @Given("Test | echo toto")
+    public void test6() {
+        System.out.println("Test");
+    }
+
+    // @Given("Test echo test < /tmp")
+    // public void test7() {
+    //     System.out.println("Test");
+    // }
+
+    // @Given("Test echo test > /tmp")
+    // public void test8() {
+    //     System.out.println("Test");
+    // }
+
+    @Given("Test ^")
+    public void test9() {
+        System.out.println("Test");
     }
     @Given("Test %test%")
-    public void test3() {
-        System.err.println("Test %test%");
+    public void test10() {
+        System.out.println("Test");
     }
+
+    @Given("Test $\\{echo test}")
+    public void test11() {
+        System.out.println("Test");
+    }
+
+    // @Given("Test backslash \\\\")
+    // public void test12() {
+    //     System.out.println("Test");
+    // }
+
+    // @Given("Test slash /")
+    // public void test13() {
+    //     System.out.println("Test");
+    // }
+
+    @Given("Test $\\(echo test)")
+    public void test14() {
+        System.out.println("Test");
+    }
+
+    @Given("Test #test")
+    public void test15() {
+        System.out.println("Test");
+    }
+
+    @Given("Test 𨱏")
+    public void test16() {
+        System.out.println("Test");
+    }
+
+    @Given("Test 👩‍❤️‍💋‍👨")
+    public void test17() {
+        System.out.println("Test");
+    }
+
+    @Given("Test [test]")
+    public void test18() {
+        System.out.println("Test");
+    }
+
+    @Given("Test ^^")
+    public void test19() {
+        System.out.println("Test");
+    }
+
+    // @Given("Test "test""")
+    // public void test20(String string) {
+    //     System.out.println("Test");
+    // }
+    // @Given("Test 'test'")
+    // public void test21(String string) {
+    //     System.out.println("Test");
+    // }
+
+    // @Given("Test $test")
+    // public void test22() {
+    //     System.out.println("Test");
+    // }
+
+    @Given("Test ,.+-_~@€çé=àû*?!")
+    public void test23() {
+        System.out.println("Test");
+    }
+    @Given("Test .\\/test")
+    public void test24() {
+        System.out.println("Test");
+    }
+    
 }
--- src/test/java/testcscu4/TestCS.feature
+++ src/test/java/testcscu4/TestCS.feature
@@ -1,7 +1,28 @@
 # language: en
-Feature: Test1
+Feature: Test_cuke
 
-	Scenario: Test1
+	Scenario: Test_cuke
+		Given test '
 		Given Test ` echo test `
-		Given Test '
+		Given Test $test
+		Given Test & echo toto
+		Given Test ; echo toto
+		Given Test | echo toto
+		# Given Test echo test < /tmp
+		# Given Test echo test > /tmp
+		Given Test ^
 		Given Test %test%
+		Given Test ${echo test}
+		# Given Test backslash \\
+		# Given Test slash /
+		Given Test $(echo test)
+		Given Test #test
+		Given Test 𨱏
+		Given Test 👩‍❤️‍💋‍👨
+		Given Test [test]
+		Given Test ^^
+		# Given Test "test"
+		# Given Test 'test'
+		# Given Test \$test
+		Given Test ,.+-_~@€çé=àû*?!
+		Given Test ./test
